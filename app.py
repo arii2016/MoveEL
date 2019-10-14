4bcc258a0ea7b7962e6ce78d9fd4e2c0e16b8a21
arii2016 <yoshiya2010@gmail.com>
2019-10-14 18:31:06 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+
+import sys, os, time
+import serial
+import Tkinter
+
+
+root = Tkinter.Tk()
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+
+
+Bt_Move = Tkinter.Button(root, text='実行', width=8, height=10, font=("", 32))
+Bt_Move.pack(expand=True)
+
+root.mainloop()
