7f54509692243caae8c0329f269e01a7d3f0ae33
arii2016 <yoshiya2010@gmail.com>
2019-10-14 18:32:22 +0800

    fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ root = Tkinter.Tk()
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 
-Bt_Move = Tkinter.Button(root, text='実行', width=8, height=10, font=("", 32))
+Bt_Move = Tkinter.Button(root, text='実行', width=8, height=5, font=("", 32))
 Bt_Move.pack(expand=True)
 
 root.mainloop()
